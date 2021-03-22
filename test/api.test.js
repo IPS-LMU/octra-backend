30c11f1806ac0c64230e9c88d8529566a1ba3956
Julian Pömp <j.poemp@campus.lmu.de>
2021-03-22 11:48:44 +0100

    replaced bcryptjs with crypto-js, improved test script

--- test/api.test.js
+++ test/api.test.js
@@ -5,6 +5,7 @@ process.env.NODE_ENV = 'test';
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require("../build/app.dev.js");
+let assert = chai.assert;
 chai.should();
 
 
@@ -15,6 +16,9 @@ const tempData = {
         addedID: 0,
         removedID: 0,
     },
+    user: {
+        name: "Julian_" + Date.now()
+    },
     jwtToken: ""
 };
 const appToken = "c817cda2c5f7635e8f5dd49c3d7958ad32bfdbd2";
@@ -24,7 +28,7 @@ describe('User', () => {
         describe('/POST v1/user/register', () => {
             it('it should POST a new user registration', (done) => {
                 const request = {
-                    "name": "Julian_" + Date.now(),
+                    "name": tempData.user.name,
                     "password": "Password1234"
                 }
 
@@ -34,9 +38,9 @@ describe('User', () => {
                     .set("Origin", "http:localhost:8080")
                     .send(request)
                     .end((err, res) => {
-                        if (!anyErrors(err, res)) {
-                            tempData.jwtToken = res.body.token;
-                        }
+                        checkForErrors(err, res);
+                        tempData.jwtToken = res.body.token;
+
                         res.body.status.should.be.equal("success");
                         res.body.should.be.a('object');
                         done();
@@ -49,8 +53,7 @@ describe('User', () => {
         describe('/POST v1/login', () => {
             it('it should POST a user login', (done) => {
                 const request = {
-                    "name": "Julian",
-                    "email": "somemail@email.de",
+                    "name": tempData.user.name,
                     "password": "Password1234"
                 }
                 chai.request(server)
@@ -59,6 +62,7 @@ describe('User', () => {
                     .set("Origin", "http:localhost:8080")
                     .send(request)
                     .end((err, res) => {
+                        checkForErrors(err, res);
                         res.body.status.should.be.equal("success");
                         res.body.should.be.a('object');
                         done();
@@ -86,10 +90,10 @@ if (true) {
                         .set("x-access-token", tempData.jwtToken)
                         .send(request)
                         .end((err, res) => {
-                            if (!anyErrors(err, res)) {
-                                tempData.apptoken.addedID = res.body.data.id;
-                                log(`added ${res.body.data.id}`);
-                            }
+                            checkForErrors(err, res);
+                            tempData.apptoken.addedID = res.body.data.id;
+                            log(`added ${res.body.data.id}`);
+
                             res.status.should.be.equal(200);
                             res.body.should.be.a('object');
                             done();
@@ -107,9 +111,9 @@ if (true) {
                         .set("Origin", "http:localhost:8080")
                         .set("x-access-token", tempData.jwtToken)
                         .end((err, res) => {
-                            if (!anyErrors(err, res)) {
-                                log(`removedRows: ${res.body.data.removedRows}`);
-                            }
+                            checkForErrors(err, res);
+                            log(`removedRows: ${res.body.data.removedRows}`);
+
                             res.status.should.be.equal(200);
                             res.body.should.be.a('object');
                             done();
@@ -127,9 +131,9 @@ if (true) {
                         .set("Origin", "http:localhost:8080")
                         .set("x-access-token", tempData.jwtToken)
                         .end((err, res) => {
-                            if (!anyErrors(err, res)) {
-                                log(`retrieved rows: ${res.body.data.length}`);
-                            }
+                            checkForErrors(err, res);
+                            log(`retrieved rows: ${res.body.data.length}`);
+
                             res.status.should.be.equal(200);
                             res.body.data.should.be.a('array');
                             done();
@@ -145,19 +149,8 @@ function log(str) {
 }
 
 
-function anyErrors(err, res) {
-    if (res.body && res.body.status === "error") {
-        log(`Error from body: ${res.body.message}`);
-        return true;
-    }
-    if (err) {
-        log(`Error from err: ${err}`);
-        return true;
-    }
-    if (res.error) {
-        log(`Error from response: ${res.error.message}`);
-        return true;
-    }
-
-    return false;
+function checkForErrors(err, res) {
+    assert.equal(err, undefined, err);
+    res.body.status.should.be.equal('success', res.body.message);
+    assert.equal(res.error, false, res.error.message);
 }
