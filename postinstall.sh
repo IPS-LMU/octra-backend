4b4f7dcd17802062ce504e697788f48362efc9fc
Julian Pömp <j.poemp@campus.lmu.de>
2022-06-11 09:14:50 +0200

    updated postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -2,3 +2,4 @@ version="v5.0.8"
 
 curl -LJO "https://github.com/TryGhost/node-sqlite3/releases/download/${version}/napi-v6-win32-unknown-x64.tar.gz"
 tar -xf "napi-v6-win32-unknown-x64.tar.gz" -C "node_modules/sqlite3/lib/binding"
+rm napi-v6-win32-unknown-x64.tar.gz
